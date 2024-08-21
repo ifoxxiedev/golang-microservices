871d8cb6a204df12dd59fdb767fcf1f226cf41f6
Autobots - First Deploy <devops@rankmyapp.com.br>
2024-08-21 12:14:24 -0300

    feat: authenticate

--- authentication-service/cmd/api/handlers.go
+++ authentication-service/cmd/api/handlers.go
@@ -1,41 +1,40 @@
 package main
 
-import "net/http"
-
-// Oww craaazy, this is a JSON payload
-type requestPayload struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-// Oww craaazy, this is a JSON payload
-type response struct {
-	Token string `json:"token"`
-}
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
 
 func (app *Config) authenticate(w http.ResponseWriter, r *http.Request) {
-	var payload requestPayload
-
 	var requestPayload struct {
 		Email    string `json:"email"`
 		Password string `json:"password"`
 	}
 
-	err := app.readJson(w, r, &payload)
-
+	err := app.readJson(w, r, &requestPayload)
 	if err != nil {
-
+		app.errorJson(w, err, http.StatusBadRequest)
+		return
 	}
 
-	user, err := app.Models.User.GetByEmail(payload.Email)
+	user, err := app.Models.User.GetByEmail(requestPayload.Email)
 	if err != nil {
-
+		app.errorJson(w, err, http.StatusBadRequest)
+		return
 	}
 
-	matches, err := app.Models.User.PasswordMatches(payload.Password)
-	if err != nil {
-
+	valid, err := app.Models.User.PasswordMatches(requestPayload.Password)
+	if err != nil || !valid {
+		app.errorJson(w, errors.New("Invalid Credentials"), http.StatusBadRequest)
+		return
 	}
 
-	// TODO - Generate JWT token
+	responsePayload := jsonResponse{
+		Error:   false,
+		Message: fmt.Sprintf("Logger in user %s", user.Email),
+		Data:    user,
+	}
+
+	app.writeJson(w, http.StatusAccepted, responsePayload)
 }
