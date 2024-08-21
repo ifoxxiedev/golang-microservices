c7fe2519404c593f011d33d21523a3016d426fdc
Autobots - First Deploy <devops@rankmyapp.com.br>
2024-08-21 16:51:02 -0300

    feat: integrate auth service with log service

--- authentication-service/cmd/api/handlers.go
+++ authentication-service/cmd/api/handlers.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func (app *Config) authenticate(w http.ResponseWriter, r *http.Request) {
@@ -30,6 +33,13 @@ func (app *Config) authenticate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// log authentification
+	err = app.logRequest("authentification", fmt.Sprintf("%s logged in", user.Email))
+	if err != nil {
+		app.errorJson(w, err)
+		return
+	}
+
 	responsePayload := jsonResponse{
 		Error:   false,
 		Message: fmt.Sprintf("Logger in user %s", user.Email),
@@ -37,4 +47,32 @@ func (app *Config) authenticate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	app.writeJson(w, http.StatusAccepted, responsePayload)
+}
+
+func (app *Config) logRequest(name, data string) error {
+	var entry struct {
+		Name string `json:"name"`
+		Data string `json:"data"`
+	}
+
+	entry.Name = name
+	entry.Data = data
+
+	jsonData, err := json.MarshalIndent(entry, "", "\t")
+	logServiceUrl := fmt.Sprintf("%s/log", os.Getenv("LOGGER_SERVICE_URL"))
+
+	request, err := http.NewRequest("POST", logServiceUrl, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+
+	if err != nil {
+		return err
+	}
+
+	defer response.Body.Close()
+	return nil
 }
