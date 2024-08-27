b7e09f9290c2c54258703616167d1b3eb87d3993
Autobots - First Deploy <devops@rankmyapp.com.br>
2024-08-27 18:00:29 -0300

    chore: consumer

---
+++ listener-service/event/consumer.go
@@ -0,0 +1,28 @@
+package event
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Consumer struct {
+	conn      *amqp.Connection
+	queueName string
+}
+
+func NewConsumer(conn *amqp.Connection, queueName string) (Consumer, error) {
+	consumer := Consumer{
+		conn,
+		queueName,
+	}
+
+	return consumer, nil
+}
+
+func (consumer *Consumer) setup() error {
+	channel, err := consumer.conn.Channel()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
