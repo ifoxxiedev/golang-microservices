748d5c851c5f726dfc6b9cd8a9f43af7aa6715a3
Autobots - First Deploy <devops@rankmyapp.com.br>
2024-08-27 17:21:21 -0300

    feat: connect rabbitmq

---
+++ listener-service/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	// try to connect rabbitmq
+	rabbitConn, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	defer rabbitConn.Close()
+
+	// start listening for messages
+
+	// create consumer
+
+	// watch the queue and consume events
+}
+
+func connect() (*amqp.Connection, error) {
+	// connect to rabbitmq
+	var counts int64
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
+
+	// don't continue until we have a connection
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+		if err != nil {
+			fmt.Println("RabbitMQ not yeat ready", backOff)
+			counts++
+		} else {
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(backOff), 2)) * time.Second
+		log.Println("backing off", backOff)
+		time.Sleep(backOff)
+		continue
+	}
+
+	log.Println("Connected to RabbitMQ")
+	return connection, nil
+}
